5796ef6173ed01cecddd5af23ff166053e98a5c0
hdoh-ucsd <103155925+hdoh-ucsd@users.noreply.github.com>
2022-06-05 22:14:52 -0700

    Lab Report 10 failing tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -54,4 +54,54 @@ public class MarkdownParseTest {
         ArrayList<String> expected = new ArrayList(List.of("https://something.com", "some-thing.html"));
         assertEquals(expected,MarkdownParse.getLinks(contents));
     }
+    @Test
+    public void testSnippet1() throws IOException {
+        String filename = "snippet1.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("`google.com", "google.com","ucsd.edu"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testSnippet2() throws IOException {
+        String filename = "snippet2.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("a.com", "a.com(())","example.com"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testSnippet3() throws IOException {
+        String filename = "snippet3.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void test500() throws IOException {
+        String filename = "500.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("#fragment", "http://example.com#fragment", "http://example.com?foo=3#frag"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void test501() throws IOException {
+        String filename = "501.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("[foo\bar]"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void test579() throws IOException {
+        String filename = "579.md";
+        Path path2filename = Paths.get(filename);
+        String contents = Files.readString(path2filename);
+        ArrayList<String> expected = new ArrayList(List.of("<url>"));
+        assertEquals(expected,MarkdownParse.getLinks(contents));
+    }
 }
+
